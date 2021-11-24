3f1452578f4c74367e2d08f9feeb453b006f8bfe
Juan Diego Castellanos <juan.castellanosj@usantoto.edu.co>
2021-11-23 20:11:00 -0500

    add datastructures and datatypes in R

---
+++ Castessoft_R_datastructures.R
@@ -0,0 +1,19 @@
+#Vector
+numeric_vector <- c(1, 10, 49)
+character_vector <- c("a", "b", "c")
+
+# Complete the code for boolean_vector
+boolean_vector <- c(TRUE, FALSE, TRUE)
+
+# Poker winnings from Monday to Friday
+poker_vector <- c(140, -50, 20, -120, 240)
+
+# Roulette winnings from Monday to Friday
+roulette_vector <-c("juan", "diego", 12345) 
+roulette_vector[1]
+
+# Poker winnings from Monday to Friday
+poker_vector <- c(140, -50, 20, -120, 240)
+
+# Roulette winnings from Monday to Friday
+roulette_vector <-c(-24, -50, 100, -350, 10)
---
+++ Castessoft_R_datatypes.R
@@ -0,0 +1,29 @@
+# Decimal values like 4.5 are called numerics.
+my_numeric <- 42.5
+
+# Numbers like 42 are called integers.
+x <- 42
+
+# Boolean values (TRUE or FALSE) are called logical.
+my_logical <- TRUE
+my_logical <- FALSE
+
+# Text (or string) values are called characters
+my_character <- "universe"
+
+# Declare variables of different types
+my_numeric <- 42
+my_character <- "universe"
+my_logical <- FALSE 
+
+# Text (or string) values are called characters
+my_character <- "universe"
+
+# Check class of my_numeric
+class(my_numeric)
+
+# Check class of my_character
+class(my_character)
+
+# Check class of my_logical
+class(my_logical)
